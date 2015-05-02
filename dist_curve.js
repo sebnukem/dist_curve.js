d8ea2aecd5bb2fa94a8eaf322b9a1a95d7277242
Sebastien Nicoud <sebnukem@gmail.com>
2015-05-01 21:43:25 -0600

    wip

--- dist_curve.js
+++ dist_curve.js
@@ -1,7 +1,14 @@
-var canvas, $canvas, ctx;
+var DISTCURV = (function($, undefined){
+
+var canvas, ctx;
+var wc = 500, hc = 500;
+var wl = 100, hl = 100;
+
 var points = [ {x:0,y:0}, {x:100,y:100} ];
+
 function log(o) { $('#d').append(''+o+'<br>'); }
 function pr(o) { $('#o').html(''+o); }
+
 function canvasxy(event) {
 	var rect = canvas.getBoundingClientRect();
     return {
@@ -11,30 +18,62 @@ function canvasxy(event) {
 }
 function c2l(point) {
 	return {
-		x: Math.floor(point.x / 5),
-		y: 100 - Math.floor(point.y / 5)
+		x: Math.floor(point.x * wl / wc),
+		y: hl - Math.floor(point.y * hl / hc)
 	};
 }
 function l2c(point) {
 	return {
-		x: point.x * 5,
-		y: 500 - point.y * 5
+		x: point.x * wc / wl,
+		y: hc - point.y * hc / hl
 	}
 }
-function mouse_down(event) {
-	var coords = c2l(canvasxy(event));
-	pr('mouse down @ '+coords.x+','+coords.y);
-}
-function draw_curve(points) {
+function sort_points(points) {
 
 }
+function mouse_down(event) {
+	var point = c2l(canvasxy(event));
+	pr('mouse down @ '+point.x+','+point.y);
+	points.push(point);
+	points.sort(function(a,b){ return b.x - a.x; });
+	draw_curve(points);
+}
+function draw_curve(points) {
+	var pp;
+	ctx.clearRect(0,0,wc,hc);
+	ctx.lineWidth = 3;
+	ctx.lineCap = 'round';
+	ctx.beginPath();
+	_.forEach(points, function(point) {
+		var p = l2c(point);
+		if (!pp) {
+			ctx.moveTo(p.x,p.y);
+		} else if (p.x == pp.x) {
+			ctx.moveTo(p.x,p.y);
+		} else {
+			ctx.lineTo(p.x,p.y);
+			//log('lineto '+p.x+','+p.y);
+		}
+		pp = p;
+	});
+	ctx.stroke();
+}
 function init() {
-	$canvas = $('#canvas');
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext("2d");
 	canvas.addEventListener("mousedown", mouse_down, false);
+
+   
+
+	draw_curve(points);
 }
+return {
+	log: log,
+	init: init
+}
+})(jQuery);
+
 $(document).ready(function() {
-	log('jquery loaded.');
-	init();
+	DISTCURV.log('jquery loaded.');
+	DISTCURV.init();
 });
